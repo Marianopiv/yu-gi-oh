bcfeb3d0ce8a9041381c63cc546bc70aef52d054
Marianopiv <marianopividori93@gmail.com>
2023-01-07 22:10:45 -0300

    agregada paginacion

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,18 +1,32 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import IndividualCards from "../individualCards/IndividualCards";
 import { CardsProvContext } from "../provider/CardsProv";
 import loader from "./loader.png";
 import "./CardList.css";
-import arrows from "../../arrows.png"
+import arrows from "../../arrows.png";
+
+const ITEMS_PER_PAGE = 50; // Number of items to display per page
+
+const pagination = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+
 const CardList = () => {
   const { data, clear } = useContext(CardsProvContext);
   const [name, setName] = useState(null);
-
-  
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   const handleChange = (name) => {
     setName(name.toLowerCase());
   };
+
+  const handlePageChange = (pageNumber) => {
+    setPage(pageNumber);
+  };
+
+  useEffect(() => {
+    data&&
+    setTotalPages(Math.ceil(data.length / ITEMS_PER_PAGE));
+  }, [data]);
 
   return (
     <>
@@ -38,17 +52,20 @@ const CardList = () => {
             name ? (
               data
                 .filter((item) => ((item.name).toLowerCase()).includes(name))
+                .slice(ITEMS_PER_PAGE * (page - 1), ITEMS_PER_PAGE * page)
                 .map((item) => (
                   <>
                     <IndividualCards item={item} />
                   </>
                 ))
             ) : (
-              data.map((item) => (
-                <>
-                  <IndividualCards item={item} clear={clear} />
-                </>
-              ))
+              data
+                .slice(ITEMS_PER_PAGE * (page - 1), ITEMS_PER_PAGE * page)
+                .map((item) => (
+                  <>
+                    <IndividualCards item={item} clear={clear} />
+                  </>
+                ))
             )
           ) : (
             <>
@@ -59,6 +76,7 @@ const CardList = () => {
           )}
         </div>
       </div>
+      <div className="flex justify-center gap-2 flex-wrap mx-2">{pagination.map((item)=><button className="border-2 rounded-md p-1" onClick={()=>handlePageChange(item)}>{item}</button>)}</div>
     </>
   );
 };
